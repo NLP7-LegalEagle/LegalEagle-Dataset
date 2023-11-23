992376b2d8339da5d4b1806fed61624c4f7a70d5
Jeonhui <dlwjsgml02@naver.com>
2023-11-23 23:25:53 +0900

    feat: update dataset

--- datasetM.py
+++ datasetM.py
@@ -4,12 +4,31 @@ class DatasetM:
     def __init__(self, file):
         self.file_paths = file
 
-    def merge(self):
+    def merge(self, max_length=None):
         for file in self.file_paths:
             df = self.read_csv(file)
-            df['result'] = df.apply(lambda df: f"<s>[INST] {df['input']} [/INST] {df['output']} </s>", axis=1)
+            df['result'] = df.apply(lambda df: DatasetM.format_instruction(df), axis=1)
             df = self.drop_columns(df, ['input', 'output'])
-            self.save_csv(df, file)
+            if max_length is not None:
+                mask = df.apply(lambda x: len(x[0]) < max_length, axis=1)
+                df = df[mask]
+            self.save_csv(df, file, max_length)
+
+    @staticmethod
+    def format_input_output(sample):
+        return f"<s>[INST] {sample['input']} [/INST] {sample['output']} </s>"
+
+    @staticmethod
+    def format_instruction(sample):
+        return f"""### Instruction:
+    Use the Input below to create an instruction, which could have been used to generate the input using an LLM.
+
+    ### Input:
+    {sample['input']}
+
+    ### Response:
+    {sample['output']}
+    """
 
     def read_csv(self, file):
         return pd.read_csv(file)
@@ -17,10 +36,13 @@ class DatasetM:
     def drop_columns(self, df, columns):
         return df.drop(columns=columns)
     
-    def save_csv(self, df, file_path):
-        file_name = "./datasetsMerge/" + file_path.split('/')[2] 
+    def save_csv(self, df, file_path, tag):
+        file_name = file_path.split('/')[2].split('.')[0]
+        if tag is not None:
+            file_name += f"_{tag}"
+        file_path = "./instruction_datasets/" + file_name +".csv"
         #print(file_name)
-        df.to_csv(file_name, index=False)
+        df.to_csv(file_path, index=False)
     
 
 file_path = ["./datasets/dataset_test.csv",
@@ -29,4 +51,4 @@ file_path = ["./datasets/dataset_test.csv",
 
 
 data_processor = DatasetM(file_path)
-data_processor.merge()
+data_processor.merge(1024)
